19f1c6ca15cea3027aa79031fba89c2bc4e9840d
Gagoi <Gagoi@192.168.1.75>
2016-11-22 20:53:10 +0100

    Suppression des derniers problèmes liés au clavier.

--- fr/gagoi/pwal/graphics/window/Display.java
+++ fr/gagoi/pwal/graphics/window/Display.java
@@ -23,12 +23,13 @@ public class Display extends JFrame implements Runnable {
 		setHeight(height);
 		canvas = new AppCanvas(width, height);
 		add(canvas);
-		canvas.requestFocus();
+		addKeyListener(keyboard);
 		canvas.addKeyListener(keyboard);
 		setSize(width, height);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
+
 		new Thread(this).start();
 	}
 
@@ -65,26 +66,24 @@ public class Display extends JFrame implements Runnable {
 
 	@Override
 	public void run() {
+		long timer = System.currentTimeMillis(), start_time_FPS = System.currentTimeMillis();
+		int tempFPS = 0;
 		while (isRunning) {
-			long timer = System.currentTimeMillis(), start_time_FPS = System.currentTimeMillis();
-			int tempFPS = 0;
-			while (isRunning) {
-				long current_time = System.currentTimeMillis();
-				if (current_time >= start_time_FPS + fpsInv) {
-					canvas.render();
-					tempFPS++;
-					start_time_FPS = System.currentTimeMillis();
-				}
-				if (System.currentTimeMillis() >= timer + 1000) {
-					timer += 1000;
-					current_fps = tempFPS;
-					tempFPS = 0;
-				}
-				try {
-					Thread.sleep((long) fpsInv);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+			long current_time = System.currentTimeMillis();
+			if (current_time >= start_time_FPS + fpsInv) {
+				canvas.render();
+				tempFPS++;
+				start_time_FPS = System.currentTimeMillis();
+			}
+			if (System.currentTimeMillis() >= timer + 1000) {
+				timer += 1000;
+				current_fps = tempFPS;
+				tempFPS = 0;
+			}
+			try {
+				Thread.sleep((long) fpsInv);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
 		}
 	}
@@ -93,11 +92,11 @@ public class Display extends JFrame implements Runnable {
 		return canvas.getLayers();
 	}
 
-	public int getCurrentFps(){
+	public int getCurrentFps() {
 		return current_fps;
 	}
-	
-	public Keyboard getKeyboard(){
+
+	public Keyboard getKeyboard() {
 		return this.keyboard;
 	}
 }
